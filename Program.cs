3f2116b183822f9835df3c174ddf8177fbb67e55
zaramen68 <mizaramen@gmail.com>
2021-04-15 08:39:10 +0300

    кабинет

--- Program.cs
+++ Program.cs
@@ -14,9 +14,15 @@ namespace testWork
  
            public static void ParceData(string source)
                 {
-                        string pattern = @"\b\p{Lu}\w*";
-                        Regex regex = new Regex(pattern);
-                        MatchCollection matches = regex.Matches(source);
+                        Dictionary<string, string> reStruct = new Dictionary<string, string>();
+                        string patternName = @"\b\p{Lu}\w*";
+                        string patternWorkTime = @"\d{2}-\d{2}";
+                        string patternRestTime = @"\d{2}-\d{2}";
+            string cab = @"\bкаб\w*|\bпомещ\w*";
+            Regex regexCab = new Regex(cab);
+            MatchCollection matchCab = regexCab.Matches(source);
+                        Regex regexName = new Regex(patternName);
+                        MatchCollection matchesName = regexName.Matches(source);
 
                         string[] parceString = source.Split(',');
                         foreach (string item in parceString)
@@ -29,7 +35,19 @@ namespace testWork
                                     break;
                                 case 2:
                                     // может быть имя фамилия или кабинет
-                                    bool res = Regex.IsMatch(source, pattern);
+                                    bool res = Regex.IsMatch(item, patternName);
+                                    if (res)
+                                    {
+                                        reStruct.Add("Работник:", item);
+                                    }
+                                    else
+                                    {
+                                        if(Regex.IsMatch(parceItem[0], @"\d+")&&Regex.IsMatch(parceItem[1], @"^каб\w* | ^пом\w*"))
+                                        {
+                                            reStruct.Add("Кабинет:", parceItem[0]);
+                                        }
+                                        
+                                    }
                                     break;
                             }
                         }
@@ -39,7 +57,7 @@ namespace testWork
        
         static void Main(string[] args)
         {
-            string str = "Иванов Иван, 97 кабинет, пн-ср, 06:00-15:00, 11:00-12:00";
+            string str = "Иванов Иван,97 кабинет помещение,пн-ср,06:00-15:00,11:00-12:00";
             ParceData(str);
 
         }
