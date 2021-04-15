eba00e9c664ed1ae2ac187d848a515d8a1f23bf2
zaramen68 <mizaramen@gmail.com>
2021-04-15 09:17:34 +0300

    last

--- Program.cs
+++ Program.cs
@@ -13,51 +13,47 @@ namespace testWork
     {
  
            public static void ParceData(string source)
-                {
-                        Dictionary<string, string> reStruct = new Dictionary<string, string>();
-                        string patternName = @"\b\p{Lu}\w*";
-                        string patternWorkTime = @"\d{2}-\d{2}";
-                        string patternRestTime = @"\d{2}-\d{2}";
+           {
+            Dictionary<string, string> reStruct = new Dictionary<string, string>();
+            string patternName = @"\b\p{Lu}\w*";
+            string patternWorkTime = @"\d{2}-\d{2}";
+            string patternRestTime = @"\d{2}-\d{2}";
+            string patterDays = @"\w{2} "
             string cab = @"\bкаб\w*|\bпомещ\w*";
+            string cabNum = @"\d+";
             Regex regexCab = new Regex(cab);
-            MatchCollection matchCab = regexCab.Matches(source);
-                        Regex regexName = new Regex(patternName);
-                        MatchCollection matchesName = regexName.Matches(source);
+            Regex regexCabNum = new Regex(cabNum);
+            
+            Regex regexName = new Regex(patternName);
+            
 
-                        string[] parceString = source.Split(',');
-                        foreach (string item in parceString)
-                        {
-                            string[] parceItem = item.Split(' ');
-                            switch (parceItem.Length)
-                            {
-                                case 1:
-                                   // проверить на дату, время
-                                    break;
-                                case 2:
-                                    // может быть имя фамилия или кабинет
-                                    bool res = Regex.IsMatch(item, patternName);
-                                    if (res)
-                                    {
-                                        reStruct.Add("Работник:", item);
-                                    }
-                                    else
-                                    {
-                                        if(Regex.IsMatch(parceItem[0], @"\d+")&&Regex.IsMatch(parceItem[1], @"^каб\w* | ^пом\w*"))
-                                        {
-                                            reStruct.Add("Кабинет:", parceItem[0]);
-                                        }
-                                        
-                                    }
-                                    break;
-                            }
-                        }
-
+            string[] parceString = source.Split(',');
+            foreach (string item in parceString)
+                {
+                MatchCollection matchesName = regexName.Matches(source);
+                if (matchesName.Count == 2)
+                {
+                    reStruct.Add("Работник:", item);
+                    continue;
                 }
+
+                MatchCollection matchCab = regexCab.Matches(source);
+                MatchCollection matchCabNum = regexCabNum.Matches(source);
+                if (matchCab.Count == 1 && matchCabNum.Count == 1)
+                {
+                    reStruct.Add("Кабинет:", matchCabNum.ToString());
+                    continue;
+                }
+
+               
+            }
+
+    }
         
        
         static void Main(string[] args)
         {
-            string str = "Иванов Иван,97 кабинет помещение,пн-ср,06:00-15:00,11:00-12:00";
+            string str = "Иванов Иван, 97 кабинет помещение, пн-ср, 06:00-15:00, 11:00-12:00";
             ParceData(str);
 
         }
