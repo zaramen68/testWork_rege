e041040977de7928eade22ece086826e43e3573f
zaram <zaramenskiy@awada.ru>
2021-04-15 14:04:42 +0300

    last

--- Program.cs
+++ Program.cs
@@ -12,49 +12,101 @@ namespace testWork
     class Program
     {
  
-           public static void ParceData(string source)
+           public static string ParceData(string source)
            {
+            int n = 0;
+            string workTime ="";
+            DateTime startWTime = new DateTime();
+            DateTime finishWTime = new DateTime();
             Dictionary<string, string> reStruct = new Dictionary<string, string>();
             string patternName = @"\b\p{Lu}\w*";
-            string patternWorkTime = @"\d{2}-\d{2}";
+            string patternTime = @"\d{2}\:\d{2}";
             string patternRestTime = @"\d{2}-\d{2}";
-            string patterDays = @"\w{2} "
+            string patterDays = @"\p{Ll}{2}|\p{Ll}{2}-\p{Ll}{2}";
             string cab = @"\bкаб\w*|\bпомещ\w*";
             string cabNum = @"\d+";
             Regex regexCab = new Regex(cab);
             Regex regexCabNum = new Regex(cabNum);
-            
+            Regex regexDays = new Regex(patterDays);
             Regex regexName = new Regex(patternName);
+            Regex regexTime = new Regex(patternTime);
             
 
             string[] parceString = source.Split(',');
             foreach (string item in parceString)
                 {
-                MatchCollection matchesName = regexName.Matches(source);
+                MatchCollection matchesName = regexName.Matches(item);
                 if (matchesName.Count == 2)
                 {
                     reStruct.Add("Работник:", item);
                     continue;
                 }
 
-                MatchCollection matchCab = regexCab.Matches(source);
-                MatchCollection matchCabNum = regexCabNum.Matches(source);
+                MatchCollection matchCab = regexCab.Matches(item);
+                MatchCollection matchCabNum = regexCabNum.Matches(item);
                 if (matchCab.Count == 1 && matchCabNum.Count == 1)
                 {
-                    reStruct.Add("Кабинет:", matchCabNum.ToString());
+                    foreach (Match mat in matchCabNum){
+                        reStruct.Add("Кабинет:", mat.Value);
+                    }
+                    
                     continue;
                 }
 
-               
-            }
+                MatchCollection matchDays = regexDays.Matches(item);
+                if (matchDays.Count > 0)
+                {
+                    reStruct.Add("Рабочие дни:", item);
+                    continue;
+                }
+                MatchCollection matchTime = regexTime.Matches(item);
+                if (matchTime.Count == 2)
+                {
+                    string[] timeList = item.Split('-');
+                    if (n == 0)
+                    {
+                        workTime = item;
+                        startWTime = DateTime.Parse(timeList[0]);
+                        finishWTime = DateTime.Parse(timeList[1]);
+                        n++;
+                        
+                    }
+                    else
+                    {
+                        if (DateTime.Parse(timeList[0]) > startWTime)
+                        {
+                            reStruct.Add("Время работы:", workTime);
+                            reStruct.Add("Обеденное время:", item);
 
-    }
+                        }
+                        else
+                        {
+                            reStruct.Add("Время работы:", item);
+                            reStruct.Add("Обеденное время:", workTime);
+                        }
+                    }
+
+                    continue;
+
+                }
+
+          
+            }
+            //string resultStr = "Работник:" + reStruct["Работник:"] + "/n" + "Кабинет:" + reStruct["Кабинет:"] + "/n" + "Рабочие дни:" +
+            //        reStruct["Рабочие дни:"] + "/n" + "Время работы:" + reStruct["Время работы:"] + "/n" + "Обеденное время:" + reStruct["Обеденное время:"];
+            //string resultStr = @"Работник: {reStruct["Работник:"]} /n Кабинет: {reStruct["Кабинет:"]} /n Рабочие дни: { reStruct["Рабочие дни:"]} /n Время работы: { reStruct["Время работы:"]}  /n Обеденное время: { reStruct["Обеденное время:"]}";
+            string resultStr = "Работник:" + reStruct["Работник:"] + '\n' + "Кабинет:" + reStruct["Кабинет:"] + '\n' + "Рабочие дни:" +
+                    reStruct["Рабочие дни:"] + '\n' + "Время работы:" + reStruct["Время работы:"] + '\n' + "Обеденное время:" + reStruct["Обеденное время:"];
+            return resultStr;
+            }
         
        
         static void Main(string[] args)
         {
-            string str = "Иванов Иван, 97 кабинет помещение, пн-ср, 06:00-15:00, 11:00-12:00";
-            ParceData(str);
+            string str = "Иванов Иван, 97 кабинет, пн-ср, 06:00 - 15:00, 11:00-12:00";
+            string result = ParceData(str);
+            Console.WriteLine(result);
+            Console.WriteLine(str);
 
         }
     }
